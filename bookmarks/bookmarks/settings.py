05c0e1144bcaa3a1b6822aa77ea6dd0564c97c80
Michal Urbanczyk <michal.urbanczyk@aptiv.com>
2021-03-20 13:28:47 +0100

    User authentication via email added

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -114,6 +115,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
