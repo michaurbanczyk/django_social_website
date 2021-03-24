1d7eeafad71b59824d916bc20305cdb83cda5de3
Michal Urbanczyk <michal.urbanczyk@aptiv.com>
2021-03-24 21:37:32 +0100

    AJAX added

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,8 +1,12 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.http import require_POST
 from .models import Image
 from django.contrib import messages
 from .forms import ImageCreateForm
+from common.decorators import ajax_required
 
 
 @login_required
@@ -31,10 +35,50 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    print(image)
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
                    'image': image})
 
 
+@ajax_required
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == "like":
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({"status": 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'ok'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request,
+                      'images/image/list_ajax.html',
+                      {"section": 'images',
+                       'images': images})
+    return render(request,
+                  'images/image/list.html',
+                  {"section": 'images',
+                   'images': images})
